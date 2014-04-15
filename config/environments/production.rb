689228967d9e655a9a94be0c9524f71a63fc60e7
erezshmuely <erezshmuely@gmail.com>
2014-04-16 01:19:49 +0300

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ ActionMailer::Base.smtp_settings = {
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
